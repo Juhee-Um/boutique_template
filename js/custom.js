125ed0a6d07df625bc120b5427c5029558e7761c
Juhee-Um <juhee.um13@gmail.com>
2023-08-09 12:51:18 +0900

    slider section

--- js/custom.js
+++ js/custom.js
@@ -28,3 +28,15 @@ function btnHoverEffect(obj) {
 }
 
 btnHoverEffect(btnObj);
+
+// Swiper plugin codes
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
