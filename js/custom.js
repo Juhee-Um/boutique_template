90ce7ccd7664e047c2c5f68f377fcbe284a5f2b0
Juhee-Um <juhee.um13@gmail.com>
2023-08-21 12:48:20 +0900

    category section responsive

--- js/custom.js
+++ js/custom.js
@@ -47,3 +47,39 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+//mobile header toggle active
+//1. 아이콘 클릭 -> 2.아이콘에 'on' 클래스 추가(toggle) -> 3.네비게이션 높이 저장 -> 4.'on'클래스가 있을 때 네비게이션 활성화 -> 5.'on'클래스가 없을 때 네비게이션 비활성화
+
+const menuIcon = document.querySelector('.menu-icon'); //메뉴 아이콘 요소 저장
+const navi = document.querySelector('.navi'); //네비게이션 박스 요소 저장
+//const에 저장하면 램에 저장되고 앞으로 자바에서 가져다 쓸 요소가 됨. 잘 저장되었는지는 console.log로 확인
+//구조는 const 이름 = document.querySelector('.html의 클래스 이름')
+
+// console.log('menuIcon', menuIcon);
+// console.log('navi', menuIcon); //const 잘 들어갔는지 확인용. 나중에 sql 데이터 가져올 때 안 가져와진다 그러면 콘솔에서 뭐가 잘못되었는지 확인한대~
+
+menuIcon.addEventListener('click', function () {
+  this.classList.toggle('on'); //여기서 this는 menuIcon임. menu-icon클릭 할때마다 on 클래스 추가 및 제거
+
+  const navHeight = navi.scrollHeight; //navi 박스의 자식요소를 포함한 높이
+  console.log(navHeight);
+
+  if (this.classList.contains('on')) {
+    navi.style.height = navHeight + 'px';
+  } else {
+    navi.style.height = 0;
+  }
+});
+//구조: 저장된 const이름.addEventListner('할 행동', 변화 함수(){})
+
+// If PC size browser, navigation height to normal
+window.addEventListener('resize', function () {
+  const winWidth = window.outerWidth; // 윈도우 화면 가로 사이즈 저장
+  if (winWidth > 980) {
+    menuIcon.classList.remove('on');
+    navi.style.height = 'auto';
+  } else {
+    navi.style.height = 0;
+  }
+});
